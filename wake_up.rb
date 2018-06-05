0d1ad1aaa39bf22e112837ade91706763ffe0d9c
Natalia0727 <oicafn@gmail.com>
2018-06-05 16:48:14 +0000

    automatically backed up by learn

--- wake_up.rb
+++ wake_up.rb
@@ -1,1 +1,13 @@
-def 
+def wake_up
+  puts "shut off alarm "
+  puts "pick out clothes for the day"
+  puts "brush teeth"
+  puts "get in shower"
+  puts "shower"
+  puts "get out of shower and dry off"
+  puts "get dressed"
+  puts "brush and do hair"
+  puts "do makeup"
+  puts "eat breakafast"
+end
+wake_up

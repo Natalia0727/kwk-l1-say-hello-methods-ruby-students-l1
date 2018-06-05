b5b28bcb6f6f04523b9ebdbf8d5f6704e1b08c8f
Natalia0727 <oicafn@gmail.com>
2018-06-05 17:18:24 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,7 @@
 # Build your say_hello method here
+def greeting(local_greeting,your_name,my_name,time_of_day)
+  
+  puts "#{local_greeting} #{your_name}, im #{my_name}, hows your #{time_of_day}?"
+  
+end
+greeting("whats up","kim","kanye","morning")

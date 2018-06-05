92e3b583b75fdf5bcf471c0c84d66a9302a9903f
Natalia0727 <oicafn@gmail.com>
2018-06-05 17:11:02 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -1,4 +1,6 @@
 def greeting(local_greeting,your_name,my_name,time_of_day)
+  
   puts "#{local_greeting} #{your_name}, im #{my_name}, hows your #{time_of_day}?"
+  
 end
 greeting("hi","alex","Natalia","morning")
